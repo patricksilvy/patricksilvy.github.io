3a62cb7ce8be663fd6eca2962bf66d2baff82182
Patrick Silvy <pat28910oshi@gmail.com>
2022-07-24 13:28:34 -0300

    nav mobile

---
+++ index.js
@@ -0,0 +1,86 @@
+const iconMobileActivate = document.querySelector('.activeMobileNav')
+const iconMobileNotActivate = document.querySelector('.notActiveMobileNav')
+const mobileIcon = document.querySelector('#mobileIcon')
+const menu  = document.querySelector('.menu')
+const skills = document.querySelectorAll('.table-skills article')
+const linksMenu = document.querySelectorAll('.menu ul li a')
+
+let activeMobileNav = false
+
+mobileIcon.addEventListener("click", () => mobileNav())
+
+window.addEventListener('resize', setWindowSize);
+window.addEventListener('scroll', hideMenu)
+
+const activate = (element) => element.style.display = 'flex'
+const notActivate = (element) => element.style.display = 'none'
+
+skills.forEach(skill => {
+    skill.addEventListener("mouseenter", () => skill.style.transform = "scale(1.2)")
+    skill.addEventListener("mouseleave", () => skill.style.transform = "scale(1)")
+})
+
+
+function mobileNav() {
+    if(activeMobileNav) {
+        activate(iconMobileActivate)
+        notActivate(iconMobileNotActivate)
+        activate(menu)
+    } else {
+        activate(iconMobileNotActivate)
+        notActivate(iconMobileActivate)
+        notActivate(menu)
+    }
+
+    return activeMobileNav = !activeMobileNav
+}
+
+let prevScrollpos = window.pageYOffset;
+
+function hideMenu() {
+    let currentScrollPos = window.pageYOffset;
+
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector('.menu-mobile').style.top = '0';
+        document.querySelector(".menu").style.top = "0";
+
+    } else {
+        document.querySelector('.menu-mobile').style.top = '-50px';
+        document.querySelector(".menu").style.top = "-50px";
+
+        if(!activeMobileNav) {
+            document.querySelector(".menu").style.top = "-180px";
+        }
+    }
+
+    prevScrollpos = currentScrollPos;
+}
+
+function setWindowSize() {
+    if (typeof (window.innerWidth) == 'number') {
+        myWidth = window.innerWidth;
+        myHeight = window.innerHeight;
+    } else {
+        if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+            myWidth = document.documentElement.clientWidth;
+            myHeight = document.documentElement.clientHeight;
+        } else {
+            if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
+                myWidth = document.body.clientWidth;
+                myHeight = document.body.clientHeight;
+            }
+        }
+    }   
+
+    if(myWidth >= 780) {
+        mobileNav()
+        return activeMobileNav = true
+    } else {
+        linksMenu.forEach(link => {
+            link.onclick = () => mobileNav()
+        })
+    }
+}
+
+mobileNav()
+setWindowSize()
